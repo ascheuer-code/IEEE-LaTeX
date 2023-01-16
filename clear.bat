db8317f2bf9edb6b00c79d25398bbc5d22ec9b30
Irrenhauspatient <andreasscheuer85@gmx.de>
2023-01-16 14:05:37 +0100

    .

---
+++ clear.bat
@@ -0,0 +1,21 @@
+del *.aux
+del *.toc
+del *.out
+del *.bak
+del *.brf
+del *.blg
+del *.bbl
+del *.lof
+del *.lot
+del *.lol
+del *.dvi
+del *.idx
+del *.ind
+del *.glo
+del *.gls
+del Chapters\*.aux
+del FrontBackMatter\*.aux
+rem del *.ps.gz
+rem del *.ps
+del *.log
+
